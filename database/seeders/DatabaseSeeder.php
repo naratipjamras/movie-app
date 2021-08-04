2f0e057dfdc3b49a2f3c01b376b28aaca5807af3
Naratip Jamras <mr.naratip.jamras@gmail.com>
2021-08-05 06:51:03 +0700

    fix files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(TagsTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        //duplicate product for data 
         $this->call(ProductsTableSeeder::class);
     }
 }
