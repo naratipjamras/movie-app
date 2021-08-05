39b3a7c9138c72b21490f7f308281acafb11ec7e
Naratip Jamras <mr.naratip.jamras@gmail.com>
2021-08-05 07:57:01 +0700

    fix files

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -14,97 +14,99 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Movie')->insert(array(
-            0 => array(
+        DB::table('Movie')->truncate();
+        $movies =[
+            [
                 'name' => 'Rampage แรมเพจ ใหญ่ชนยักษ์',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=rampage-2018',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/Rampage-2018-1.jpg',
                 'youtube' => 'DHmptNyIuGQ',
-            ),
-            1 => array(
+            ],
+            [
                 'name' => 'San Andreas มหาวินาศแผ่นดินแยก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=san-andreas-2015',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/AR7OrBwGDThJPHWmzH9.jpg',
                 'youtube' => 'yftHosO0eUo',
-            ),
-            2 => array(
+            ],
+            [
                 'name' => 'The Day After Tomorrow วิกฤติวันสิ้นโลก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=the-day-after-tomorrow-2004-sub',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/LqKGfMbIIGul9Xnh6Mf.jpg',
                 'youtube' => 'Ku_IseK3xTc',
-            ),
-            3 => array(
+            ],
+            [
                 'name' => '2012 วันสิ้นโลก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=2012-2009',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2021/03/2021.jpg',
                 'youtube' => 'gEI7FJIdRFc',
-            ),
-            4 => array(
+            ],
+            [
                 'name' => 'Independence Day ไอดี 4 สงครามวันดับโลก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=independence-day-1996',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/nN7oRkgXhK5yZMLMY9F.jpg',
                 'youtube' => 'S3ce1Cvig-M',
-            ),
-            5 => array(
+            ],
+            [
                 'name' => 'Exodus: Gods and Kings เอ็กโซดัส : ก็อดส์ แอนด์ คิงส์',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=exodus-gods-and-kings-2014',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/8WPyIlaevn3TEMAnzbK.jpg',
                 'youtube' => '8YsulfxVI',
-            ),
-            6 => array(
+            ],
+            [
                 'name' => 'The Core ผ่านรกกลางใจโลก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=the-core-2003',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/VuBVLaCdtallhPQgXX7.jpg',
                 'youtube' => 'LVkocdkcmAc',
-            ),
-            7 => array(
+            ],
+            [
                 'name' => 'True Legend ยาจกซู',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=true-legend-2010',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/e3Vmd1VhnNURX9kYS1U.jpg',
                 'youtube' => 'gLgnZYNf84c',
-            ),
-            8 => array(
+            ],
+            [
                 'name' => 'THE 5TH WAVE (2016) อุบัติการณ์ล้างโลก',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=the-5th-wave-2016',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/ayq5f7pknuv1x7aAMmf.jpg',
                 'youtube' => 'kmxLybfGNC4',
-            ),
-            9 => array(
+            ],
+            [
                 'name' => 'AQUAMAN (2018) อควาแมน เจ้าสมุทร',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=aquaman-2018',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/12/Aquaman-2018.jpg',
                 'youtube' => '2wcj6SrX4zw',
-            ),
-            10 => array(
+            ],
+            [
                 'name' => 'DOCTOR STRANGE ด็อกเตอร์ สเตรนจ์ จอมเวทย์มหากาฬ',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=doctor-strange-2016',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/nET8xOVdi7MgpeF8jcL.jpg',
                 'youtube' => 'HSzx-zryEgM',
-            ),
-            11 => array(
+            ],
+            [
                 'name' => 'THE MARTIAN (2015) กู้ตาย 140 ล้านไมล์',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=the-martian-2015',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2018/04/McBMPYoF1WBmzfgr2xt.jpg',
                 'youtube' => 'ej3ioOneTy8',
-            ),
-            12 => array(
+            ],
+            [
                 'name' => 'WONDER WOMAN (2017) วันเดอร์ วูแมน',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=wonder-woman-2017',
                 'image' => 'https://www.037hdmovie.com/wp-content/uploads/2020/02/Wonder-Woman-2017-%E0%B8%A7%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%8C-%E0%B8%A7%E0%B8%B9%E0%B9%81%E0%B8%A1%E0%B8%99.jpg',
                 'youtube' => '1Q8fG0TtVAY',
-            ),
-            13 => array(
+            ],
+            [
                 'name' => 'Pearl Harbor',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=pearl-harbor-2001',
                 'image' => 'https://m.media-amazon.com/images/M/MV5BMTQ3MDc0MDc1NF5BMl5BanBnXkFtZTYwODI1ODY2._V1_SX300.jpg',
                 'youtube' => 'oGYcxjywx0o',
-            ),
-            14 => array(
+            ],
+            [
                 'name' => 'Titanic',
                 'link' => 'https://p2p-ns1.sage.stream/index.html?id=titanic-1997',
                 'image' => 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg',
                 'youtube' => 'CHekzSiZjrY',
-            ),
+            ],
         ));
+        DB::table('Movie')->insert($movies);
     }
 }
