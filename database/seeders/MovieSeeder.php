a0ea964ac18ec75a848401669b3bb1a593ac62c8
aom <mr.naratip.jamras@gmail.com>
2021-08-18 13:47:42 +0700

    fix files

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -106,7 +106,7 @@ class MovieSeeder extends Seeder
                 'image' => 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg',
                 'youtube' => 'CHekzSiZjrY',
             ],
-        ));
+        ];
         DB::table('Movie')->insert($movies);
     }
 }
